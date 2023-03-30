63b85882ec8814327c8869a2b8f580b1536facdb
zengks <stevevitasks@hotmail.com>
2023-03-29 23:08:52 -0700

    added in all the routing features

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Successfully connected to the database!'))
 
 const userSchema = new Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  }
 })
 
 const exerciseSchema = new Schema({
-  username: String,
-  description: String,
-  duration: Number,
-  date: String
+  userId: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, min: 1, required: true },
+  date: { type: Date, default: Date.now }
 })
 
 const User = mongoose.model('User', userSchema)
@@ -46,13 +50,86 @@ app.get('/api/users/', async (req, res) => {
 
 app.get('/api/users/:_id/logs', async (req, res) => {
   const userId = req.params._id
-  let userLogs = await Exercise.find({ _id: userId })
-  console.log(userLogs)
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  const from = dateRegex.test(req.query.from) ? new Date(req.query.from).toDateString() : ''
+  const to = dateRegex.test(req.query.to) ? new Date(req.query.to) : ''
+  const limit = req.query.limit
+  try {
+    let findUser = await User.findById(userId)
+    if (findUser) {
+      let findUserExercise = await Exercise.find({ userId })
+      if (findUserExercise) {
+        let filteredLogs = []
+        let limitedCount
+        if (from !== undefined || to !== undefined || !isNaN(limit)) {
+          findUserExercise.forEach(exe => {
+            let logDate = new Date(exe.date)
+            if (from instanceof (Date) && to instanceof (Date)) {
+              if (logDate >= from && logDate <= to) {
+                filteredLogs.push({
+                  description: exe.description,
+                  duration: exe.duration,
+                  date: new Date(exe.date).toDateString()
+                })
+              }
+            } else if (from instanceof (Date)) {
+              if (logDate >= from) {
+                filteredLogs.push({
+                  description: exe.description,
+                  duration: exe.duration,
+                  date: new Date(exe.date).toDateString()
+                })
+              }
+            } else if (to instanceof (Date)) {
+              if (logDate <= to) {
+                filteredLogs.push({
+                  description: exe.description,
+                  duration: exe.duration,
+                  date: new Date(exe.date).toDateString()
+                })
+              }
+            }
+          })
+          if (limit !== '' && !isNaN(limit)) {
+            const sorted = filteredLogs.sort((a, b) => a - b)
+            limitedCount = sorted.slice(0, limit)
+          }
+          return res.json({
+            _id: findUser._id,
+            username: findUser.username,
+            count: findUserExercise.length,
+            logs: limit > 0 ? limitedCount : filteredLogs,
+          })
+        } else {
+          return res.json({
+            _id: findUser._id,
+            username: findUser.username,
+            count: findUserExercise.length,
+            logs: findUserExercise
+          })
+        }
+      }
+    } else {
+      return res.json({
+        error: 'User not found!'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.json({
+      error: 'Something wrong with the server...Related to Logs'
+    })
+  }
 })
 
 app.post('/api/users/', async (req, res) => {
   const username = req.body.username
   try {
+    if (username === '') {
+      return res.json({
+        error: 'Username is required'
+      })
+    }
     let findUser = await User.findOne({
       username
     })
@@ -83,24 +160,40 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
   const userId = req.params._id
   const description = req.body.description
   const duration = req.body.duration
-  const date = req.body.date ? new Date(req.body.date) : new Date()
+  const date = req.body.date ? new Date(req.body.date).toDateString() : new Date()
   try {
-    let findUser = User.findById(userId)
-    if (findUser !== null) {
-      let newExercise = new Exercise({
-        date,
-        username: findUser.username,
-        duration,
-        description
-      })
-      newExercise._id = userId
-      await newExercise.save()
+    if (description === '') {
       return res.json({
-        _id: newExercise._id,
-        username: newExercise.username,
-        date: new Date(newExercise.date).toDateString(),
-        duration: newExercise.duration,
-        description: newExercise.description
+        error: 'Description field is required'
+      })
+    }
+    if (isNaN(duration)) {
+      return res.json({
+        error: 'Duration field cannot be empty and must be a valid Number'
+      })
+    }
+    if (date instanceof Date) {
+      return res.json({
+        error: 'Date field cannot be empty and must be a valid Date'
+      })
+    }
+    let findUser = await User.findById(userId)
+    if (findUser !== null) {
+      let newUserExercise = new Exercise({
+        userId,
+        description,
+        duration,
+        date,
+      })
+      console.log(date)
+      await newUserExercise.save().then((data) => {
+        res.json({
+          _id: data['userId'],
+          username: data['username'],
+          description: data['description'],
+          duration: data['duration'],
+          date: new Date(data['date']).toDateString()
+        })
       })
     } else {
       return res.json({
@@ -114,7 +207,6 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
     })
   }
 })
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
